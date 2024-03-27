0d07954724503f254457c83d1e29a75382fd03f2
alexandratarod <alexandratarod@yahoo.ro>
2024-03-27 21:18:16 +0200

    Add routes

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+
 dotenv.config();
 
 mongoose
@@ -11,6 +17,14 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+
+  app.use(express.json());
+  app.use("/auth", authRoute);
+  app.use("/users", userRoute);
+  app.use("/products", productRoute);
+  app.use("/cart", cartRoute);
+  app.use("/orders", orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
